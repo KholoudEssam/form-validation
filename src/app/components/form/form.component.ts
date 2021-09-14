2291c8bf9e2dc2a5c1535c0064081325a0e8c7fe
Kholoud Essam <kholoud.essamfciscu@gmail.com>
2021-09-14 22:21:18 +0200

    adding form validation

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -10,15 +17,46 @@ export class FormComponent {
   signupForm: FormGroup;
 
   constructor() {
-    this.signupForm = new FormGroup({
-      name: new FormControl(null),
-      email: new FormControl(null),
-      password: new FormControl(null),
-      confirmPassword: new FormControl(null),
-    });
+    this.signupForm = new FormGroup(
+      {
+        name: new FormControl(null, [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.pattern(/^[a-z]+$/i),
+        ]),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+        password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+        confirmPassword: new FormControl(null, [Validators.required]),
+      },
+      { validators: isNotMatched }
+    );
+  }
+
+  get name() {
+    return this.signupForm.get('name');
+  }
+
+  get email() {
+    return this.signupForm.get('email');
+  }
+
+  get password() {
+    return this.signupForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.signupForm.get('confirmPassword');
   }
 
   onSubmitForm() {
-    console.log('form submitted');
+    console.log(this.signupForm.value);
+    this.signupForm.reset();
   }
 }
+
+const isNotMatched: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const pwd = control.get('password')?.value;
+  const cPwd = control.get('confirmPassword')?.value;
+
+  return pwd && cPwd && pwd !== cPwd ? { isNotMatched: true } : null;
+};
